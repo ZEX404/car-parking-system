4114cc745d783c85d17dd53532c18424d44a0656
Ziad Mohamed <ziad.salama@ejust.edu.eg>
2023-09-06 12:27:39 +0200

    added UART

--- APP/main.c
+++ APP/main.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include "../LIB/STD_TYPES.h"
 #include "../LIB/BIT_MATH.h"
-#include "../MCAL/DIO.h"
-#include "../HAL/CLCD.h"
-#include "../HAL/Keypad.h"
+#include "../MCAL/DIO/DIO.h"
+#include "../HAL/CLCD/CLCD.h"
+#include "../HAL/Keypad/Keypad.h"
 #include <util/delay.h>
 #include <avr/io.h>
 
