499aecf2cb0d3f9516ba00c678d9eb58c89e329e
Ahmed Anwar Gad <49812918+ahmedanwar123@users.noreply.github.com>
2023-09-13 13:39:32 +0200

    Added Day Night heck Part

--- Graduation_Final_RX_1/main_RX.c
+++ Graduation_Final_RX_1/main_RX.c
@@ -2,6 +2,7 @@
 #include "BIT_MATH.h"
 #include "DIO.h"
 #include "Servo.h"
+#include "LDR.h"
 #include "UART_RX.h"
 #include <avr/io.h>
 #include <util/delay.h>
@@ -11,11 +12,14 @@ int main(void){
     DIO_SetPinDirection(DIO_PORTD, DIO_PIN5, DIO_PIN_OUTPUT); // OC1A (Timer1) for servo
     DIO_SetPinDirection(DIO_PORTD, DIO_PIN0, DIO_PIN_INPUT);
     DIO_SetPinDirection(DIO_PORTD, DIO_PIN1, DIO_PIN_OUTPUT);
+    // Set pin directions for LDR
+
 	UART_voidINIT_RX();
     TIMER1_voidCTCINIT();
     TIMER1_voidSetTopTicks(20000);
     u8 recieved_data = UART_u8RecieveData_RX();
     while(1){
+    	LDR();
     	recieved_data = UART_u8RecieveData_RX();
     	if(recieved_data == SIGNAL_OPENGATE){
     		Servo_OpenGate();
